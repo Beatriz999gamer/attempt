5d772611e007bc4c440814eb69d4c816ae525cc2
Beatriz999gamer <potatoesbeatriz@gmail.com>
2021-03-26 13:21:46 +0000

    hello

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-
-
-function integrate (f, start, end, step) {
-    let total = 0
-    step = step || 0.01
-    for (let x = start; x < end; x += step) {
-      total += f(x + step / 2) * step
-    }
-    return total
+function integrate(f, bottom, top) {
+  let area = 0
+  let dx = 0.001
+  for(let x = bottom; x < top; x += dx) {
+    area += dx * f(x)
   }
+  return area
+}
+console.log(integrate(x => x, 0, 5).toFixed(3))
 
-  function gamma(a) {
-      x**(a-1) * math.e**(-x)
+function gamma(z) {
+  function g(x) {
+    return x**z * Math.exp(-x)
   }
+  return g
+}
 
-  function factorial(x) {
-      return integrate(gamma,0,math.Infinity)
-  }
+function factorial(x) {
+  return integrate(gamma(x),0,50000)
+}
 
-  console.log(factorial(1))
-  console.log(factorial(0.5))
+console.log(factorial(5).toFixed(3))
